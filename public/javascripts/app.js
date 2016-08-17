b3cfc6a76287175b58389bfa9eb3a7ce83edb22f
ivan.sabelnikov <ivan.sabelnikov@gmail.com>
2016-08-17 21:53:45 +0300

    initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,11 @@
+﻿app = angular.module('angularModule', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+            when('/main', {
+                templateUrl: 'partials/main.html'
+            }).
+            when('/fileManager', {
+                templateUrl: 'partials/fileManager.html'
+            }).
+            otherwise({ redirectTo: '/main' });
+    }]);
---
+++ public/javascripts/controllers.js
@@ -0,0 +1,7 @@
+﻿app.controller('MainController', ['$scope', function ($scope) {
+    var socket = io.connect('http://localhost');
+    socket.on('news', function (data) {
+        alert(data);
+        socket.emit('my other event', { my: 'data' });
+    });
+}]);
