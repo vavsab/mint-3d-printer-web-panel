e3a75ba2db5ff90d571ab959142631977b724225
ivan.sabelnikov <ivan.sabelnikov@gmail.com>
2016-08-30 01:23:35 +0300

    Add printer emulator

---
+++ KeepCalmPrinterEmulator/src/PrintDream.PrinterEmulator/ProcessController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace PrintDream.Core
+{
+    public class ProcessController
+    {
+        public event EventHandler<string> DataReceived;
+
+        public string Path { get; set; }
+
+        public string FileName { get; set; }
+
+        public Process Start()
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = System.IO.Path.Combine(Path, FileName),
+                RedirectStandardOutput = true
+            };
+
+            var process = 
+                Process.GetProcessesByName(FileName).FirstOrDefault() 
+                ?? Process.Start(startInfo);
+
+            process.BeginOutputReadLine();
+            process.OutputDataReceived += (sender, eventArgs) =>
+            {
+                DataReceived?.Invoke(this, eventArgs.Data);
+            };
+
+            return process;
+        }
+    }
+}
